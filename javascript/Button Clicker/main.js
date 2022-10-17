123ea316e61adff38ab8b083d8e7039ad70f2327
John Doe <johndoe@example.com>
2022-10-17 11:40:03 -0700

    Week_2_day_1

---
+++ javascript/Button Clicker/main.js
@@ -0,0 +1,16 @@
+// Remove the Add Definition Button
+function remove(element){
+    element.remove()
+}
+
+// login button change
+var navbtm = "Logout"
+function buttonClick(element){
+    element.innerText = `Logout`
+}
+
+// Alert Buttons
+
+function showAlert(){
+    alert("Ninja was liked")
+}
